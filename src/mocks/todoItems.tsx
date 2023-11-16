e8cdac2cb6b7e106fe72f3a630d687c3cf4cc643
Antonio Mesquita <antonio.mesquita@ifood.com.br>
2023-11-16 15:44:48 -0300

    feat: add todoitems mock and render list to home page

---
+++ src/mocks/todoItems.tsx
@@ -0,0 +1,44 @@
+
+type TodoItem = {
+    id: Number;
+    description: String;
+    title: String;
+    updated_at: String;
+    created_at: String;
+    author: Number;
+}
+
+export const todoItemsMock: TodoItem[] = [
+    {
+      id: 9,
+      description: "Fourth description",
+      title: "Fourth title",
+      updated_at: "2023-11-15T17:42:55.107203Z",
+      created_at: "2023-11-15T17:42:55.107194Z",
+      author: 1
+    },
+    {
+      id: 6,
+      description: "Second description edited",
+      title: "Second title edited",
+      updated_at: "2023-11-15T17:14:28.088753Z",
+      created_at: "2023-11-15T17:06:00.425931Z",
+      author: 1
+    },
+    {
+      id: 8,
+      description: "Third description",
+      title: "Third title",
+      updated_at: "2023-11-15T17:13:36.086236Z",
+      created_at: "2023-11-15T17:13:36.086229Z",
+      author: 1
+    },
+    {
+      id: 7,
+      description: "First description",
+      title: "First title",
+      updated_at: "2023-11-15T17:11:49.832162Z",
+      created_at: "2023-11-15T17:11:49.832155Z",
+      author: 1
+    }
+  ]
